c8ec4e72d45f388d056c633391331a23edab0a3d
Shakeel Faiz <shakeel.faiz@aspose.com>
2018-07-21 17:30:08 +0500

    Center Align Excel Cell

--- Center Align Excel Cell/Center_Align_Excel_Cell.cs
+++ Center Align Excel Cell/Center_Align_Excel_Cell.cs
@@ -4,11 +4,11 @@ Aspose.Cells.Workbook wb = new Aspose.Cells.Workbook();
 // Access first worksheet.
 Worksheet ws = wb.Worksheets[0];
 
-// Access cell C4 and some text inside it.
+// Access cell C4 and add some text inside it.
 Cell c4 = ws.Cells["C4"];
 c4.PutValue("Aspose File Format APIs");
 
-// Access cell style
+// Access cell style.
 Style st = c4.GetStyle();
 
 // Set the horizontal and vertical alignment of the cell style.
@@ -18,10 +18,10 @@ st.VerticalAlignment = TextAlignmentType.Center;
 // Set the font color to red.
 st.Font.Color = Color.Red;
 
-// Set the cell style
+// Set the cell style.
 c4.SetStyle(st);
 
-//Set the cell's column width and row height.
+// Set the cell's column width and row height.
 ws.Cells.SetColumnWidth(c4.Column, 30);
 ws.Cells.SetRowHeight(c4.Row, 40);
 
